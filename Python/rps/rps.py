608e1aec1d2a29f9361efd8c16f285d9994ea027
Sheri Michetti <sherimichetti@gmail.com>
2018-09-10 21:03:51 -0700

    Expand game with lizard, spock

--- Python/rps/rps.py
+++ Python/rps/rps.py
@@ -6,12 +6,12 @@ and reports both Player's scores each round."""
 # import random module for random.choice element
 import random
 
-moves = ['rock', 'paper', 'scissors']
+moves = ['rock', 'paper', 'scissors', 'lizard', 'spock']
 
 
 # Creates main Player class. All subclass players inherit from this class
 class Player:
-    #
+    # Player always returns move rock
     def move(self):
         return 'rock'
 
@@ -28,10 +28,8 @@ class RandomPlayer(Player):
 
 # Subclass player that copies the opponent's last move
 class ReflectPlayer(Player):
-    """
-    Initializes player with a random move if player goes first since there
-    will not be a previous move to call
-    """
+
+    # Starts player with a random move since no previous move to call
     def __init__(self):
         self.opponents_last_move = random.choice(moves)
 
@@ -53,7 +51,7 @@ class CyclePlayer(Player):
     # Index last move to determine list placement and identify next move
     def move(self):
         index = moves.index(self.my_last_move)
-        if index == 2:
+        if index == 6:
             return moves[0]
         else:
             return moves[index + 1]
@@ -75,7 +73,8 @@ class Human(Player):
 
         while True:
             Human_move = input(
-                "Choose your weapon: rock, paper or scissors\n").lower()
+                "Choose your weapon: rock, paper, scissors, lizard or spock: "
+            ).lower()
             # Incorrect response - prompted to enter a new move
             if Human_move.lower() not in moves:
                 print("Oops, I don't recognize that weapon. Please try again")
@@ -88,8 +87,15 @@ class Human(Player):
 # Function to define what move beats another
 def beats(one, two):
     return ((one == 'rock' and two == 'scissors') or
+            (one == 'rock' and two == 'lizard') or
+            (one == 'scissors' and two == 'lizard') or
             (one == 'scissors' and two == 'paper') or
-            (one == 'paper' and two == 'rock'))
+            (one == 'paper' and two == 'rock') or
+            (one == 'paper' and two == 'spock') or
+            (one == 'lizard' and two == 'paper') or
+            (one == 'lizard' and two == 'spock') or
+            (one == 'spock' and two == 'rock') or
+            (one == 'spock' and two == 'scissors'))
 
 
 class Game:
@@ -110,23 +116,23 @@ class Game:
             self.p1wins += 1
             print(f"{move1} beats {move2}! Player 1 wins this round!")
             print("Game Totals:")
-            print(f" You: {self.p1wins}")
-            print(f" Computer: {self.p2wins}")
+            print(f" Player One: {self.p1wins}")
+            print(f" Player Two: {self.p2wins}")
             print(f" Ties: {self.ties}")
         # Reverse inputs to determine if its a tie
         elif beats(move2, move1):
             self.p2wins += 1
             print(f"{move2} beats {move1}! Player 2 wins this round!")
             print("Game Totals:")
-            print(f" You: {self.p1wins}")
-            print(f" Computer: {self.p2wins}")
+            print(f" Player One: {self.p1wins}")
+            print(f" Player Two: {self.p2wins}")
             print(f" Ties: {self.ties}")
         else:
             self.ties += 1
             print("It's a tie!")
             print("Game Totals:")
-            print(f" You: {self.p1wins}")
-            print(f" Computer: {self.p2wins}")
+            print(f" Player One: {self.p1wins}")
+            print(f" Player Two: {self.p2wins}")
             print(f" Ties: {self.ties}")
 
     # Displays final message signifying the end of the game with final scores
@@ -156,13 +162,35 @@ class Game:
         print("~~~~~~~~~~~~~~~~ PREPARE FOR BATTLE! ~~~~~~~~~~~~~~~~")
         print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n")
         # Identify round in range
-        for round in range(1, 4):
+        for round in range(1, 8):
             print(f"Round {round}: ")
             self.play_round()
         print("\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
         self.show_final_score()
 
+    # Welcome to the game
+    def welcome(self):
+        option = input(
+            "\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+            "\n~~   WELCOME TO ROCK, PAPER, SCISSORS, LIZARD, SPOCK!     ~~"
+            "\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+            "\n~~ Your moves: Rock, Paper, Scissors, Lizard or Spock. ~~"
+            "\n   Rules of the game:"
+            "\n     ~ Rock crushers Scissors and Lizard."
+            "\n     ~ Scissors cut Paper and decapitate Lizard."
+            "\n     ~ Lizard eats Paper and poisons Spock."
+            "\n     ~ Paper disproves Spock and covers Rock."
+            "\n     ~ Spock vaporizes Rock and smashes Scissors."
+            "\n~~ The best of 7 rounds WINS the game!"
+            "\n~~ Ready to play? Type yes or q to quit.")
+        if option == 'yes' or option == 'y':
+            self.play_game()
+        elif option == 'n':
+            print("Ok. Come back to play again soon!")
+        else:
+            print("I didn't understand your response. Try again.")
+
 
 if __name__ == '__main__':
-    game = Game(Human(), Cycle())
-    game.play_game()
+    game = Game(Human(), RandomPlayer())
+    game.welcome()
