40a4e048fec9dc3c540fb74d59e8a950f5e95327
Sheri Michetti <sherimichetti@gmail.com>
2018-09-24 18:10:37 -0700

    Update makeGrid() to show grid in HTML

--- javascript/pixelArtProject/designs.js
+++ javascript/pixelArtProject/designs.js
@@ -1,36 +1,35 @@
-// Select color input
-const chooseColor = document.querySelector("#colorPicker");
+// Call makeGrid function when user submits input
+function makeGrid() {
+	var height = document.getElementById("inputHeight").value
+	var width = document.getElementById("inputWidth").value
 
-// Select size input
-const tableHeight = document.getElementById('inputHeight');
-const tableWidth = document.getElementById('inputWidth');
-const canvas = document.getElementById('pixelCanvas');
+	//create the table
+	var myGrid = document.getElementById("pixelCanvas");
+	var tableBody = document.createElement("tbody");
+	myGrid.appendChild(tableBody);
+
+	myGrid.innerHTML = '';
+	for (var i = 0; i < height; i++) {
+  	var tr = document.createElement("tr");
+  	tableBody.appendChild(tr);
+
+  		for (var c = 0; c < width; c++) {
+  			var td = document.createElement('td');
+  			tr.appendChild(td);
+  		}
+    }
+    myGrid.appendChild(tableBody);
+    td.addEventListener('click', clickedBox);
+}
+
+
 
 // When a box is clicked, add color 
 function clickedBox(event) {
-  const color = colorInput.value;
-  event.target.style.backgroundColor = color;
+
+	const color = document.getElementById("colorPicker").value
+	event.target.style.backgroundColor = color;
 }
-
-// Call makeGrid function when user submits input
-function makeGrid() {
-  // Your code goes here!
-  canvas.innerHTML = '';
-
-  //create a table element and a tbody element
-  var table = document.getElementById("pixelCanvas");
-  for (var i = 0; i < tableHeight; i++) {
-  	var tr = document.createElement("tr");
-  	for (var c = 0; c < tableWidth; c++) {
-  	var td = document.createElement("td");
-
-    tr.addEventListener('click', clickedBox);
-    table.appendChild(tr);
-  }
-  // Add grid to the table in the DOM
-  document.body.appendChild(table);
-}
-};
 
 document.querySelector('form').addEventListener('submit', function(event){
   event.preventDefault();
